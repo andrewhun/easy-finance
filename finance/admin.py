f5d1cfdc3fcd379cdc766db6990108244468efe7
andrewhun995 <40831919+andrewhun@users.noreply.github.com>
2019-01-07 23:35:06 +0100

    remove unnecessary directory

--- finance/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Autovalue, Transaction, Portfolio, Statement, Cash
-# Register your models here.
-admin.site.register(Autovalue)
-admin.site.register(Transaction)
-admin.site.register(Portfolio)
-admin.site.register(Statement)
-admin.site.register(Cash)
